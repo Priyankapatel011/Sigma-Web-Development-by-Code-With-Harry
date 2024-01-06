2b910ced858c854dfa4147ef514aced9e8127a28
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-01-06 20:04:47 +0530

    Add files via upload

---
+++ JavaScript/Introduction/index.js
@@ -0,0 +1,15 @@
+alert("Hello All");
+
+console.log("I am good and you?");
+
+var a = prompt("Enter number");
+
+console.log(a);
+
+var isTrue = confirm("Are you sure??");  
+console.log(isTrue);                       //true/false
+
+
+document.title = "Hey I am also good";
+
+document.body.style.backgroundColor = "black";  //inline css add

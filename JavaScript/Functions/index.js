10d43c5d661ec71873047367598554f1d1b87611
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-01-07 21:46:43 +0530

    Add files via upload

--- JavaScript/Functions/index.js
+++ JavaScript/Functions/index.js
@@ -1,1 +1,48 @@
+//FUNCTIONS
 
+// function nice(name) {
+//     console.log("Helloooooooooooo " + name + " you are nice");
+//     console.log("Helloooooooooooo" + " " + name + " you can do it");
+//     console.log("Helloooooooooooo " + name + " you are confident");
+//     console.log("Helloooooooooooo " + name + " be calmer");
+    
+// }
+
+// //function call
+// nice("Priyanka");
+// nice("Pinku");
+
+
+// function sum(a, b) {
+//     // console.log(a + b);
+//     return a+b;
+// }
+// result = sum(5, 4);
+// console.log("The sum is: " + result);  //concat
+// console.log("The sum is:", result);
+
+// function sum(a, b, c=2) {
+//     // console.log(a + b);
+//     return a+b+c;
+// }
+// result = sum(5, 4);
+// console.log("The sum is: " + result);  //concat
+// console.log("The sum is:", result);
+
+// function sum(a, b, c=2) {
+//     // console.log(a + b);
+//     return a+b+c;
+// }
+// result = sum(5, 4, 10);
+// console.log("The sum is: " + result);  //concat
+// console.log("The sum is:", result);
+
+
+//ARROW Function
+
+const func = (x) => {
+    console.log("Hey I'm an arrow function...", x);
+}
+func(5)
+func(8)
+func(55)

3f95b4693dc2e693fa9a7df6fe63a76872c46f50
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-01-07 21:47:56 +0530

    Add files via upload

--- JavaScript/stringManipulations/index.js
+++ JavaScript/stringManipulations/index.js
@@ -1,1 +1,51 @@
+// let a = "priyanka";
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[6]);
+
+// console.log(a[8]);
+
+// console.log(a.length);
+
+// let friend = "Karishma";
+// let names = "Priyanka";
+
+// console.log("Hello", friend, "and", names);
+// console.log(`Hello ${friend} and ${names}`);    // string interpolation
+
+// console.log(`My name "is" Priyanka's`);
+
+// //Escape sequence characters
+// let day = 'Mother\'s day'
+// console.log(day);
+
+// // let say = "Hii\nHow are you??"
+// // console.log(say);
+
+// let s = "Hii\tHow are you??"
+// console.log(s);
+
+// let t = "Hii I'm Priyanka\rHow are you??"   //carriage return
+// console.log(t);
+
+
+// strings are immutable
+// let p = "priyanka";
+// let q = "pinku";
+// let r = "riya";
+// console.log(p.toUpperCase());
+// console.log(p.toLowerCase());
+
+
+// console.log(p.slice(1,6));
+
+// console.log(p.replace("ka", "ma"));  //only 1st occurrence
+
+// console.log(p.concat(q, r));
+
+//remove white spaces
+let a = "    apple  ";
+console.log(a);
+console.log(a.trim());
+
 

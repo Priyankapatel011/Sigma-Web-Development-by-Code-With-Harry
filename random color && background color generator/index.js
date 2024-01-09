ada6b66fa64f62a148cb846f3507d4390cc3bebb
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-01-09 21:16:56 +0530

    Add files via upload

--- random color && background color generator/index.js
+++ random color && background color generator/index.js
@@ -1,1 +1,25 @@
+Bgcolors = [
+    "pink",
+    "orange",
+    "black",
+    "purple",
+    "gray",
+    "seagreen"
+]
 
+colors = [
+    "white",
+    "blue",
+    "cyan",
+    "yellow",
+    "green"
+]
+
+let boxes = document.getElementsByClassName("box");
+let randomBgColor = Math.floor(Math.random() * Bgcolors.size()); 
+let randomColor = Math.floor(Math.random() * colors.size()); 
+
+boxes.forEach(element => {
+    element.style.backgroundColor = Bgcolors[randomBgColor];
+    element.style.color = color[randomColor];
+});

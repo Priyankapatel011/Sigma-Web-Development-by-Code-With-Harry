7586cbb49100b0a581d3aab3bc3da705f0d4856d
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-01-13 21:38:03 +0530

    Add files via upload

--- JavaScript/Advance-JS/index.js
+++ JavaScript/Advance-JS/index.js
@@ -1,1 +1,23 @@
+//IIFE
+async function sleep() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(45);
+        }, 1000);
+    })
+}
 
+let a = sleep();
+let b = sleep();
+
+
+//SPREAD
+
+
+
+
+//DESTRUCTURING
+
+
+
+//

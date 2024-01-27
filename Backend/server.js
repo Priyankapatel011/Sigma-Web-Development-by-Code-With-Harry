863eeb76895b0c6ff8ea5471bb1057e6e2cfbb3a
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-01-27 21:26:03 +0530

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,16 @@
+// console.log("Hello Everyone!!!");
+// console.log("Be happy😊😊");
 
+// So this slugify code is already written inside slugify modelu. So we don't have to write this code again Simply use it😀!!!
+
+var slugify = require('slugify');
+
+const a = slugify('Some Awesome String'); // BY Default --> slug using '-'
+console.log(a);
+
+//if we want other than '-'
+const b = slugify('Some Craziest Ideas', '&&');
+const c = slugify('Some Cr#$%^5(*&^%$ty', '&&');
+
+console.log(b);
+console.log(c);

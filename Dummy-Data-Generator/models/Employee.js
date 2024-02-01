2bfb46dee660630bdaf9277fb83ecf3441e5cbda
Priyanka Patel <128116001+Priyankapatel011@users.noreply.github.com>
2024-02-01 14:38:17 +0530

    Add files via upload

--- Dummy-Data-Generator/models/Employee.js
+++ Dummy-Data-Generator/models/Employee.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+const employeeSchema = new mongoose.Schema({
+    name: String,
+    salary: Number,
+    language: String,
+    city: String,
+    isManager: Boolean
+})
+
+
+// const Employee = mongoose.model('Employee', employeeSchema);
+// module.exports = Employee;
+export const Employee = mongoose.model('Employee', employeeSchema);
